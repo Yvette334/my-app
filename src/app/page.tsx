f1bb824a31a48641d1effe4a969a4d3e889cea5f
Yvette334 <y.muhoracye@alustudent.com>
2025-11-06 09:18:00 +0200

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
 export default function Home(){
-    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [time, setTime] = useState("");
     useEffect(()=> {
+        setTime(new Date().toLocaleTimeString());
         const interval = setInterval(()=>
             setTime(new Date().toLocaleTimeString()), 1000)
         return ()=> clearInterval(interval)
